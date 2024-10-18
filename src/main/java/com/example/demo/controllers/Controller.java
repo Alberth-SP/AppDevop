80f9aa87613d933bb57adb2f23a3395bd1cf23d1
Alberthsp <alberth.cb92@gmail.com>
2024-10-18 18:33:51 -0500

    agregando segundo endpoint

--- src/main/java/com/example/demo/controllers/Controller.java
+++ src/main/java/com/example/demo/controllers/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Date;
 @RestController
 @RequestMapping("/api/v1/saludo")
 public class Controller {
@@ -13,6 +13,11 @@ public class Controller {
 	@GetMapping("/{name}")
 	public ResponseEntity<?> saludar(@PathVariable String name){
 		return ResponseEntity.ok("Hola mundo, te damos la bienvenida " + name);
+	}
+	
+	@GetMapping("/date")
+	public ResponseEntity<?> getFecha(){
+		return ResponseEntity.ok("La fecha es " + (new Date()).toString() );
 	}
 
 }
