edcbe060ad5e9c59860716823647b8fb626ef156
Alberthsp <alberth.cb92@gmail.com>
2024-10-18 20:59:42 -0500

    agregando nuevo endpoint

--- src/main/java/com/example/demo/controllers/Controller.java
+++ src/main/java/com/example/demo/controllers/Controller.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 import java.util.Date;
 @RestController
-@RequestMapping("/api/v1/saludo")
+@RequestMapping("/api/v1")
 public class Controller {
 	
-	@GetMapping("/{name}")
+	@GetMapping("/saludar/{name}")
 	public ResponseEntity<?> saludar(@PathVariable String name){
 		return ResponseEntity.ok("Hola mundo, te damos la bienvenida " + name);
 	}
@@ -18,6 +20,13 @@ public class Controller {
 	@GetMapping("/date/current")
 	public ResponseEntity<?> getFecha(){
 		return ResponseEntity.ok("La fecha es " + (new Date()).toString() );
+	}
+	
+	@GetMapping("/date/calcularNacimiento/{anio}")
+	public ResponseEntity<?> calcularNacimiento(@PathVariable Integer anio){
+		LocalDate dateCurrent = LocalDate.now();
+		Integer edad = dateCurrent.getYear() - anio;
+		return ResponseEntity.ok("Tienes " + edad + " años");
 	}
 
 }
